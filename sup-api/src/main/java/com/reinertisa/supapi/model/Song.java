11c09e3eacfff702a205f78f84843b743e06c091
Isa Reinert <reinert.isa@gmail.com>
2025-02-07 12:17:33 -0800

    fix(api): create song entity

---
+++ sup-api/src/main/java/com/reinertisa/supapi/model/Song.java
@@ -0,0 +1,59 @@
+package com.reinertisa.supapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "songs")
+public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "artist")
+    private String artist;
+
+    public Song() {
+
+    }
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                '}';
+    }
+}
