96ce3c566b1b5202c95ebb5c6a89cf69421b02b1
Isa Reinert <reinert.isa@gmail.com>
2025-02-07 11:59:26 -0800

    feat(website): create song form component

---
+++ sup-website/src/components/song/Form.js
@@ -0,0 +1,31 @@
+import {useContext, useState} from 'react';
+import {SongContext} from "../context/SongContext";
+
+export default function SongForm() {
+    const {dispatchSongs} = useContext(SongContext);
+    const [artist, setArtist] = useState('');
+    const [title, setTitle] = useState('');
+
+    const handleArtistChange = (evt) => setArtist(evt.target.value);
+    const handleTitleChange = (evt) => setTitle(evt.target.value);
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+
+        dispatchSongs({type: 'ADD_SONG', artist, title});
+        setTitle('');
+        setArtist('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="artist">Artist</label>
+            <input id="artist" type="text" value={artist} onChange={handleArtistChange} required={true} />
+
+            <label htmlFor="title">Title</label>
+            <input id="title" type="text" value={title} onChange={handleTitleChange} required={true} />
+
+            <button type="submit">Save</button>
+        </form>
+    )
+}
