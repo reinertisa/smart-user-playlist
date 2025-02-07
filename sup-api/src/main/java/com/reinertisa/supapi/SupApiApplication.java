0b05c0b2641fb03de9276b2a2bf044cf0a178f09
Isa Reinert <reinert.isa@gmail.com>
2025-02-06 23:04:22 -0800

    feat(website): initialize project

---
+++ sup-api/src/main/java/com/reinertisa/supapi/SupApiApplication.java
@@ -0,0 +1,13 @@
+package com.reinertisa.supapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupApiApplication.class, args);
+    }
+
+}
---
+++ sup-api/src/test/java/com/reinertisa/supapi/SupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinertisa.supapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
