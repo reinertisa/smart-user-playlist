5af11407c9b694a28abbaff5148a1df67aa8a69e
Isa Reinert <reinert.isa@gmail.com>
2025-02-07 00:39:29 -0800

    fix(website): update root component

--- sup-website/src/App.js
+++ sup-website/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import SongContextProvider from "./components/context/SongContext";
+import SongList from "./components/song/List";
+import SongForm from "./components/song/Form";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <SongContextProvider>
+            <SongList />
+            <SongForm />
+        </SongContextProvider>
     </div>
   );
 }
