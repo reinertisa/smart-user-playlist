4a09e643be11dd956e05bec8f43b16d865f45508
Isa Reinert <reinert.isa@gmail.com>
2025-02-07 12:33:50 -0800

    feat(api): create song rest controller

---
+++ sup-api/src/main/java/com/reinertisa/supapi/controller/SongController.java
@@ -0,0 +1,34 @@
+package com.reinertisa.supapi.controller;
+
+
+import com.reinertisa.supapi.model.Song;
+import com.reinertisa.supapi.service.SongService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/songs")
+@CrossOrigin(origins = "*")
+public class SongController {
+    private final SongService songService;
+
+    public SongController(SongService songService) {
+        this.songService = songService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Song>> getAllSongs() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(songService.findAll());
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+        }
+    }
+}
