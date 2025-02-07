9f9c3efd0b73875f47afb234d3c2e5fcea743068
Isa Reinert <reinert.isa@gmail.com>
2025-02-06 23:50:22 -0800

    feat(website): create song details component

---
+++ sup-website/src/components/song/Details.js
@@ -0,0 +1,13 @@
+import {useContext} from 'react'
+import {SongContext} from "../context/SongContext";
+
+export default function SongDetails({song}) {
+    const {dispatchSongs} = useContext(SongContext);
+
+    return (
+        <li onClick={dispatchSongs({type: 'REMOVE_SONG', id: song.id})}>
+            <div className="artist">{song.artist}</div>
+            <div className="title">{song.title}</div>
+        </li>
+    )
+}
