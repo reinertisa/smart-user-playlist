2dd21ccf23b3551e4335d85c4eb5c42e85d3a100
Isa Reinert <reinert.isa@gmail.com>
2025-02-07 12:33:43 -0800

    feat(api): create song repository

---
+++ sup-api/src/main/java/com/reinertisa/supapi/repository/SongRepository.java
@@ -0,0 +1,9 @@
+package com.reinertisa.supapi.repository;
+
+import com.reinertisa.supapi.model.Song;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, Long> {
+}
