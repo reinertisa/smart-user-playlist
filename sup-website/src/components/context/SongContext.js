9363fca54f37fd7417edd1a4d883569a2ccd9458
Isa Reinert <reinert.isa@gmail.com>
2025-02-07 12:01:05 -0800

    fix(website): add songs to local storage

--- sup-website/src/components/context/SongContext.js
+++ sup-website/src/components/context/SongContext.js
@@ -1,11 +1,21 @@
-import {createContext, useReducer} from 'react';
+import {createContext, useEffect, useReducer} from 'react';
 import songReducer from "../reducer/songReducer";
 
 export const SongContext = createContext();
 
 export default function SongContextProvider({children}) {
 
-    const [songs, dispatchSongs] = useReducer(songReducer, []);
+    const [songs, dispatchSongs] = useReducer(songReducer, [], () => {
+        const localData = window.localStorage.getItem('songs');
+        if (localData) {
+            return JSON.parse(localData);
+        }
+        return [];
+    });
+
+    useEffect(() => {
+        window.localStorage.setItem('songs', JSON.stringify(songs));
+    }, [songs]);
 
     return (
         <SongContext.Provider value={{songs, dispatchSongs}}>
