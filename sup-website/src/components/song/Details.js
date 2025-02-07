070ab851ba8ccfbdf14e3b20dc83aa9810c45892
Isa Reinert <reinert.isa@gmail.com>
2025-02-07 00:39:17 -0800

    fix(website): update onClick handle in li tag

--- sup-website/src/components/song/Details.js
+++ sup-website/src/components/song/Details.js
@@ -5,7 +5,7 @@ export default function SongDetails({song}) {
     const {dispatchSongs} = useContext(SongContext);
 
     return (
-        <li onClick={dispatchSongs({type: 'REMOVE_SONG', id: song.id})}>
+        <li onClick={() => dispatchSongs({type: 'REMOVE_SONG', id: song.id})}>
             <div className="artist">{song.artist}</div>
             <div className="title">{song.title}</div>
         </li>
