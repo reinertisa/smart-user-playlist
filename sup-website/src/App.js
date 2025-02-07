fda1f97bd7a5c6fda0d3247afae562e609a8358a
Isa Reinert <reinert.isa@gmail.com>
2025-02-07 12:00:19 -0800

    fix(website): wrap navbar with auth context

--- sup-website/src/App.js
+++ sup-website/src/App.js
@@ -1,11 +1,16 @@
 import SongContextProvider from "./components/context/SongContext";
 import SongList from "./components/song/List";
 import SongForm from "./components/song/Form";
+import Navbar from "./components/Navbar";
+import AuthContextProvider from "./components/context/AuthContext";
 
 function App() {
   return (
     <div className="App">
         <SongContextProvider>
+            <AuthContextProvider>
+                <Navbar />
+            </AuthContextProvider>
             <SongList />
             <SongForm />
         </SongContextProvider>
