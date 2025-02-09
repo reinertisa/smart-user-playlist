8cd2f390a7aa9a2db73a0cf9d58eccccabd48209
Isa Reinert <reinert.isa@gmail.com>
2025-02-09 12:04:49 -0800

    feat(website): add fetch post submit handler to song form

--- sup-website/src/components/song/Form.js
+++ sup-website/src/components/song/Form.js
@@ -5,14 +5,39 @@ export default function SongForm() {
     const {dispatchSongs} = useContext(SongContext);
     const [artist, setArtist] = useState('');
     const [title, setTitle] = useState('');
+    const [err, setErr] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const handleArtistChange = (evt) => setArtist(evt.target.value);
     const handleTitleChange = (evt) => setTitle(evt.target.value);
 
-    const handleSubmit = (evt) => {
+    const handleSubmit = async (evt) => {
         evt.preventDefault();
 
-        dispatchSongs({type: 'ADD_SONG', artist, title});
+        try {
+            setLoading(true);
+            const rez = await fetch('http://localhost:8080/api/v1/songs', {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    title,
+                    artist,
+                })
+            });
+
+            if (!rez.ok) {
+                throw new Error('Something went wrong');
+            }
+            const json = await rez.json();
+            dispatchSongs({type: 'ADD_SONG', ...json});
+            setLoading(false);
+        } catch (err) {
+            setLoading(false);
+            setErr(err);
+        }
+
         setTitle('');
         setArtist('');
     }
@@ -37,8 +62,8 @@ export default function SongForm() {
                 required={true}
                 placeholder="artist"
             />
-
-            <input type="submit" value="Save" />
+            <input disabled={loading} type="submit" value="Save" />
+            {err && <p style={{color: 'red'}}>{err.message}</p> }
         </form>
     )
 }
