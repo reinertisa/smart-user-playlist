d467e64e9d4ac859c074d0b7ecd606ed0a62298c
Isa Reinert <reinert.isa@gmail.com>
2025-02-09 12:05:06 -0800

    fix(website): update song reducer

--- sup-website/src/components/reducer/songReducer.js
+++ sup-website/src/components/reducer/songReducer.js
@@ -3,7 +3,7 @@ import {v4 as uuid} from 'uuid';
 export default function songReducer(songs, action) {
     switch (action.type) {
         case 'ADD_SONG':
-            return [...songs, {id: uuid(), artist: action.artist, title: action.title}];
+            return [...songs, {id: action.songId || uuid(), artist: action.artist, title: action.title}];
         case 'REMOVE_SONG':
             return songs.filter(song => song.id !== action.id);
         default:
