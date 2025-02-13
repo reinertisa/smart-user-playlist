14270b4c1af512feefe36a3c25fd956b3c9c26bc
Isa Reinert <reinert.isa@gmail.com>
2025-02-13 01:36:28 -0800

    feat(website): add delete handler to components

--- sup-website/src/components/song/Details.js
+++ sup-website/src/components/song/Details.js
@@ -2,13 +2,19 @@ import {useContext} from 'react'
 import {SongContext} from "../context/SongContext";
 import {ThemeContext} from "../context/ThemeContext";
 
-export default function SongDetails({song}) {
+export default function SongDetails({song, onDelete}) {
     const {dispatchSongs} = useContext(SongContext);
     const {isLightTheme, darkMode, lightMode} = useContext(ThemeContext);
     const theme = isLightTheme ? lightMode : darkMode;
 
     return (
-        <li style={{background: theme.ui}} onClick={() => dispatchSongs({type: 'REMOVE_SONG', id: song.id})}>
+        <li
+            style={{background: theme.ui, color: theme.syntax}}
+            onClick={() => {
+                dispatchSongs({type: 'REMOVE_SONG', songId: song.songId})
+                onDelete(song.songId)
+            }}
+        >
             <div className="artist">{song.artist}</div>
             <div className="title">{song.title}</div>
         </li>
--- sup-website/src/components/song/List.js
+++ sup-website/src/components/song/List.js
@@ -1,22 +1,70 @@
-import {useContext} from 'react';
-import {SongContext} from "../context/SongContext";
+import {useContext, useEffect, useState} from 'react';
 import SongDetails from "./Details";
 import {ThemeContext} from "../context/ThemeContext";
 
 export default function SongList() {
-    const {songs} = useContext(SongContext);
     const {isLightTheme, darkMode, lightMode} = useContext(ThemeContext);
     const theme = isLightTheme ? lightMode : darkMode;
+    const [songs, setSongs] = useState([]);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
 
-    return songs?.length > 0 ? (
-        <div className="song-list" style={{color: theme.syntax, background: theme.bg}}>
-            <ul>
-                {songs.map(song => (
-                    <SongDetails key={song.id} song={song} />
-                ))}
-            </ul>
-        </div>
-    ) : (
-        <div className="empty">No song found on the list!</div>
-    )
+    useEffect(() => {
+        let mount = true;
+        const loadData = async () => {
+            try {
+                const rez = await fetch('http://localhost:8080/api/v1/songs');
+                if (!rez.ok) {
+                    throw Error('Failed to fetching data');
+                }
+                if (mount) {
+                    setSongs(await rez.json());
+                    setLoading(false);
+                    setError('');
+                }
+            } catch (err) {
+                if (mount) {
+                    setError(err.message);
+                    setLoading(false);
+                }
+            }
+        }
+
+        loadData();
+
+        return () => {
+            mount = false;
+        }
+    }, []);
+
+    const handleDelete = async (id) => {
+        try {
+            await fetch(`http://localhost:8080/api/v1/songs/${id}`, {
+                method: 'DELETE',
+            });
+            setSongs(songs.filter(song => song?.songId !== id));
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
+    let body = loading ? <div>Loading...</div> : null;
+    if (songs) {
+        console.log('songs', songs);
+        body = songs?.length > 0 ? (
+            <div className="song-list" style={{color: theme.syntax, background: theme.bg}}>
+                <ul>
+                    {songs.map(song => (
+                        <SongDetails key={song?.songId} song={song} onDelete={handleDelete} />
+                    ))}
+                </ul>
+            </div>
+        ) : (
+            <div className="empty">No song found on the list!</div>
+        )
+    } else if (error) {
+        body = <div>{error}</div>
+    }
+
+    return body;
 }
