54f7b74322f982763533231d0844cd3798d2f3bf
Isa Reinert <reinert.isa@gmail.com>
2025-03-03 02:01:41 -0800

    feat(api): add authentication provider to security config

--- sup-api/src/main/java/com/reinertisa/supapi/config/SecurityConfig.java
+++ sup-api/src/main/java/com/reinertisa/supapi/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.reinertisa.supapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +27,13 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        authProvider.setUserDetailsService(userDetailsService);
+        return authProvider;
     }
 }
