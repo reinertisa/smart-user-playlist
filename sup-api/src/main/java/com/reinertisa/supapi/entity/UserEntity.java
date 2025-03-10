9167956fd6bf51bc35d6686d1efc296fd9930b4d
Isa Reinert <reinert.isa@gmail.com>
2025-03-10 01:56:27 -0700

    fix(api): update user entity with jpa annotations

--- sup-api/src/main/java/com/reinertisa/supapi/entity/UserEntity.java
+++ sup-api/src/main/java/com/reinertisa/supapi/entity/UserEntity.java
@@ -1,12 +1,23 @@
 package com.reinertisa.supapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import java.time.LocalDateTime;
 
 
+@Entity
+@Table(name = "users")
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class UserEntity extends Auditable {
+    @Column(updatable = false, unique = true, nullable = false)
     private String userId;
     private String firstName;
     private String lastName;
+    @Column(unique = true, nullable = false)
     private String email;
     private Integer loginAttempts;
     private LocalDateTime lastLogin;
@@ -17,7 +28,159 @@ public class UserEntity extends Auditable {
     private boolean accountNonLocked;
     private boolean enabled;
     private boolean mfa;
+    @JsonIgnore
     private String qrCodeSecret;
+    @Column(columnDefinition = "text")
     private String qrCodeImageUri;
     private String roles; // TODO(reinertisa) create Role class and map here with JPA
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getLoginAttempts() {
+        return loginAttempts;
+    }
+
+    public void setLoginAttempts(Integer loginAttempts) {
+        this.loginAttempts = loginAttempts;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isMfa() {
+        return mfa;
+    }
+
+    public void setMfa(boolean mfa) {
+        this.mfa = mfa;
+    }
+
+    public String getQrCodeSecret() {
+        return qrCodeSecret;
+    }
+
+    public void setQrCodeSecret(String qrCodeSecret) {
+        this.qrCodeSecret = qrCodeSecret;
+    }
+
+    public String getQrCodeImageUri() {
+        return qrCodeImageUri;
+    }
+
+    public void setQrCodeImageUri(String qrCodeImageUri) {
+        this.qrCodeImageUri = qrCodeImageUri;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "userId='" + userId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", loginAttempts=" + loginAttempts +
+                ", lastLogin=" + lastLogin +
+                ", phone='" + phone + '\'' +
+                ", bio='" + bio + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", accountNonExpired=" + accountNonExpired +
+                ", accountNonLocked=" + accountNonLocked +
+                ", enabled=" + enabled +
+                ", mfa=" + mfa +
+                ", qrCodeSecret='" + qrCodeSecret + '\'' +
+                ", qrCodeImageUri='" + qrCodeImageUri + '\'' +
+                ", roles='" + roles + '\'' +
+                '}';
+    }
 }
