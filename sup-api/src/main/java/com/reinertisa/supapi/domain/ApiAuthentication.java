3988247069359e500005d0b7d666924431fb9fbf
Isa Reinert <reinert.isa@gmail.com>
2025-03-12 15:13:49 -0700

    feat(api): create custom api authentication class

---
+++ sup-api/src/main/java/com/reinertisa/supapi/domain/ApiAuthentication.java
@@ -0,0 +1,10 @@
+package com.reinertisa.supapi.domain;
+
+import com.reinertisa.supapi.dto.User;
+
+public class ApiAuthentication {
+    private User user;
+    private String email;
+    private String password;
+    private boolean authenticated;
+}
