a7cc20243aa9ee77bc5d85942f034b04af566dc8
Isa Reinert <reinert.isa@gmail.com>
2025-03-20 12:19:31 -0700

    fix(api): update public urls and paths

--- sup-api/src/main/java/com/reinertisa/supapi/constant/Constants.java
+++ sup-api/src/main/java/com/reinertisa/supapi/constant/Constants.java
@@ -2,14 +2,14 @@ package com.reinertisa.supapi.constant;
 
 public class Constants {
     public static final String FILE_STORAGE = System.getProperty("user.home") + "/Downloads/uploads/";
-    public static final String[] PUBLIC_URLS = { "/user/resetpassword/reset/**", "/user/verify/resetpassword/**", "/user/resetpassword/**", "/user/verify/qrcode/**", "/user/login/**", "/user/verify/account/**", "/user/register/**", "/user/new/password/**", "/user/verify/**", "/user/resetpassword/**", "/user/image/**", "/user/verify/password/**" };
+    public static final String[] PUBLIC_URLS = { "/api/v1/users/resetpassword/reset/**", "/api/v1/users/verify/resetpassword/**", "/api/v1/users/resetpassword/**", "/api/v1/users/verify/qrcode/**", "/api/v1/users/login/**", "/api/v1/users/verify/account/**", "/api/v1/users/register/**", "/api/v1/users/new/password/**", "/api/v1/users/verify/**", "/api/v1/users/resetpassword/**", "/api/v1/users/image/**", "/api/v1/users/verify/password/**" };
     public static final int NINETY_DAYS = 90;
     public static final int STRENGTH = 12;
     public static final String BASE_PATH = "/**";
     public static final String FILE_NAME = "File-Name";
     public static final String LOGIN_PATH = "/api/v1/users/login";
 
-    public static final String[] PUBLIC_ROUTES = { "/user/resetpassword/reset", "/user/verify/resetpassword", "/user/resetpassword", "/user/verify/qrcode", "/user/stream", "/user/id", "/user/login", "/user/register", "/user/new/password", "/user/verify", "/user/refresh/token", "/user/resetpassword", "/user/image", "/user/verify/account", "/user/verify/password", "/user/verify/code"};
+    public static final String[] PUBLIC_ROUTES = { "/api/v1/users/resetpassword/reset", "/api/v1/users/verify/resetpassword", "/api/v1/users/resetpassword", "/api/v1/users/verify/qrcode", "/api/v1/users/stream", "/api/v1/users/id", "/api/v1/users/login", "/api/v1/users/register", "/api/v1/users/new/password", "/api/v1/users/verify", "/api/v1/users/refresh/token", "/api/v1/users/resetpassword", "/api/v1/users/image", "/api/v1/users/verify/account", "/api/v1/users/verify/password", "/api/v1/users/verify/code"};
     public static final String AUTHORITIES = "authorities";
     public static final String GET_ARRAYS_LLC = "GET_ARRAYS_LLC";
     public static final String EMPTY_VALUE = "empty";
