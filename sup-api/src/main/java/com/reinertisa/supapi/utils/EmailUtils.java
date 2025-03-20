1cc191f2fda046d8dde83c2f2f836ddc8015747a
Isa Reinert <reinert.isa@gmail.com>
2025-03-20 12:19:46 -0700

    fix(api): update email utils

--- sup-api/src/main/java/com/reinertisa/supapi/utils/EmailUtils.java
+++ sup-api/src/main/java/com/reinertisa/supapi/utils/EmailUtils.java
@@ -3,20 +3,19 @@ package com.reinertisa.supapi.utils;
 public class EmailUtils {
 
     public static String getEmailMessage(String name, String host, String key) {
-        return "Hello " + name + ",\n\nYour new account has been created. Please click on the link below to verify " +
-                "your account.\n\n" + getVerificationUrl(host, key) + "\n\nThe Support Team";
-    }
-
-    public static String getVerificationUrl(String host, String key) {
-        return host + "/verify/account?key=" + key;
+        return "Hello " + name + "\n\nYour new account has been created. Please click on the link below to " +
+                "verify your account.\n\n" + getVerificationUrl(host, key) + "\n\nThe Support team";
     }
 
     public static String getResetPasswordMessage(String name, String host, String key) {
-        return "Hello " + name + ",\n\nYour new account has been created. Please click on the link below to verify " +
-                "your account.\n\n" + getResetPasswordUrl(host, key) + "\n\nThe Support Team";
+        return "Hello " + name + "\n\nPlease use this link below to reset your password.\n\n" + getResetPasswordUrl(host, key) + "\n\nThe Support team";
     }
 
-    public static String getResetPasswordUrl(String host, String key) {
-        return host + "/verify/password?key=" + key;
+    private static String getResetPasswordUrl(String host, String key) {
+        return host + "/api/v1/users/verify/password?key=" + key;
+    }
+
+    private static String getVerificationUrl(String host, String key) {
+        return host + "/api/v1/users/verify/account?key=" + key;
     }
 }
