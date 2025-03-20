278fa60ae435a752f6a8f87acabc691e9b368476
Isa Reinert <reinert.isa@gmail.com>
2025-03-19 20:51:25 -0700

    feat(api): update RequestUtils

--- sup-api/src/main/java/com/reinertisa/supapi/utils/RequestUtils.java
+++ sup-api/src/main/java/com/reinertisa/supapi/utils/RequestUtils.java
@@ -1,6 +1,7 @@
 package com.reinertisa.supapi.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.reinertisa.supapi.domain.Response;
 import com.reinertisa.supapi.exception.ApiException;
 import jakarta.servlet.ServletOutputStream;
@@ -8,12 +9,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.CredentialsExpiredException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.LockedException;
+import org.springframework.security.authentication.*;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -21,20 +20,21 @@ import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
+
 import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseMessage;
 
 public class RequestUtils {
 
     private static final BiConsumer<HttpServletResponse, Response> writeResponse =
-            ((httpServletResponse, response) -> {
-        try {
-            ServletOutputStream outputStream = httpServletResponse.getOutputStream();
-            new ObjectMapper().writeValue(outputStream, response);
-            outputStream.flush();
-        } catch (Exception ex) {
-            throw new ApiException(ex.getMessage());
-        }
-    });
+            (httpServletResponse, response) -> {
+                try {
+                    ServletOutputStream outputStream = httpServletResponse.getOutputStream();
+                    new ObjectMapper().writeValue(outputStream, response);
+                    outputStream.flush();
+                } catch (Exception ex) {
+                    throw new ApiException(ex.getMessage());
+                }
+            };
 
     private static final BiFunction<Exception, HttpStatus, String> errorReason =
             (exception, httpStatus) -> {
@@ -42,18 +42,17 @@ public class RequestUtils {
             return "You do not have enough permission.";
         }
         if (httpStatus.isSameCodeAs(HttpStatus.UNAUTHORIZED)) {
-            return "You do not logged in.";
+            return "You are not logged in.";
         }
-
-        if (exception instanceof DisabledException
-                || exception instanceof LockedException
-                || exception instanceof BadCredentialsException
-                || exception instanceof CredentialsExpiredException
-                || exception instanceof ApiException) {
+        if (exception instanceof DisabledException ||
+                exception instanceof LockedException ||
+                exception instanceof BadCredentialsException ||
+                exception instanceof CredentialsExpiredException ||
+                exception instanceof ApiException) {
             return exception.getMessage();
         }
         if (httpStatus.is5xxServerError()) {
-            return "An internal server error occurred.";
+            return "An internal error occurred.";
         } else {
             return "An error occurred. Please try again.";
         }
@@ -72,16 +71,45 @@ public class RequestUtils {
         );
     }
 
+    public static Response handleErrorResponse(String message, String exception, HttpServletRequest request,
+                                               HttpStatusCode status) {
+        return new Response(
+                LocalDateTime.now().toString(),
+                status.value(),
+                request.getRequestURI(),
+                HttpStatus.valueOf(status.value()),
+                message,
+                exception,
+                Collections.emptyMap());
+    }
+
     public static void handleErrorResponse(HttpServletRequest request, HttpServletResponse response,
                                            Exception exception) {
         if (exception instanceof AccessDeniedException) {
             Response apiResponse = getErrorResponse(request, response, exception, HttpStatus.FORBIDDEN);
             writeResponse.accept(response, apiResponse);
+        } else if (exception instanceof InsufficientAuthenticationException) {
+            Response apiResponse = getErrorResponse(request, response, exception, HttpStatus.UNAUTHORIZED);
+            writeResponse.accept(response, apiResponse);
+        }else if (exception instanceof MismatchedInputException) {
+            Response apiResponse = getErrorResponse(request, response, exception, HttpStatus.BAD_REQUEST);
+            writeResponse.accept(response, apiResponse);
+        }else if (exception instanceof DisabledException ||
+                exception instanceof LockedException ||
+                exception instanceof BadCredentialsException ||
+                exception instanceof CredentialsExpiredException ||
+                exception instanceof ApiException) {
+            Response apiResponse = getErrorResponse(request, response, exception, HttpStatus.BAD_REQUEST);
+            writeResponse.accept(response, apiResponse);
+        } else {
+            Response apiResponse = getErrorResponse(request, response, exception, HttpStatus.INTERNAL_SERVER_ERROR);
+            writeResponse.accept(response, apiResponse);
         }
     }
 
-    public static Response getErrorResponse(HttpServletRequest request, HttpServletResponse response,
-                                            Exception exception, HttpStatus status) {
+
+    private static Response getErrorResponse(HttpServletRequest request, HttpServletResponse response,
+                                             Exception exception, HttpStatus status) {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(status.value());
         return new Response(
@@ -90,7 +118,7 @@ public class RequestUtils {
                 request.getRequestURI(),
                 HttpStatus.valueOf(status.value()),
                 errorReason.apply(exception, status),
-                getRootCauseMessage(exception), Collections.emptyMap()
-        );
+                getRootCauseMessage(exception),
+                Collections.emptyMap());
     }
 }
